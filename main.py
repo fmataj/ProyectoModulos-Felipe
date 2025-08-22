2105a9d85acba7600b6f5583a1309f86ebd71daf
fmataj <fmataj@ctpplatanares.ed.cr>
2025-08-22 14:04:01 -0600

    cambios

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-# Proyecto: [Nombre del Proyecto]
-# Estudiante: [Nombre del Estudiante]
-# Fecha de Inicio: [dd/mm/aaaa]
-# Fecha de Entrega: [dd/mm/aaaa]
+# Proyecto: [Modulos Y Funciones de Python]
+# Estudiante: [Felipe Mata]
+# Fecha de Inicio: [22/8/2025]
+# Fecha de Entrega: []
 # Descripción: Este archivo contiene el punto de entrada principal del proyecto.
 # Recuerda incluir tu nombre completo, la fecha en la que iniciaste el proyecto y la fecha estimada de entrega.
 # Esto ayuda a mantener un registro claro del trabajo realizado.
+
+
+from analisis_datos.carga_datos import generar_lista_compras
+
+def saludar():
+    print("Hola desde la funcion")
+    
+if __name__ == "__main__":
+    generar_lista_compras()
